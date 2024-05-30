bc75a9268da5a57aed7f7f55afe9fd4d8ba5b34a
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-05-30 08:24:21 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,7 +1,6 @@
 apt-get update
-apt-get upgrade -y
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository  -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update -y 
-apt install docker.io
+apt-get update
+apt install docker.io -y
