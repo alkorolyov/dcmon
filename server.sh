3eae11585b7a464570d9f94099a3f4df01416cc9
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-05-30 08:28:43 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,6 +1,13 @@
+# docker install
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository  -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt install docker.io -y
+
+apt remove docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.24.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+wget https://raw.githubusercontent.com/jjziets/DCMontoring/main/server/docker-compose.yml
