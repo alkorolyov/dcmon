c399b4fa0e54c16a5795ef5e0d5d08ddddbb26d0
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-05-30 20:16:51 +0200

    Update and rename client.sh to install.sh

--- client/install.sh
+++ client/install.sh
@@ -3,7 +3,6 @@ curl -L "https://github.com/docker/compose/releases/download/v2.24.4/docker-comp
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 apt-get update && sudo apt-get install -y gettext-base
-wget -O docker-compose.yml https://raw.githubusercontent.com/jjziets/DCMontoring/main/client/docker-compose.yml-vast
 wget -O /usr/local/bin/check-upgradable-packages.sh  https://github.com/jjziets/gddr6_temps/raw/master/update-package-count.sh;
 chmod +x /usr/local/bin/check-upgradable-packages.sh;
 sudo bash -c '(crontab -l 2>/dev/null; echo "0 * * * * /usr/local/bin/check-upgradable-packages.sh") | crontab -'
