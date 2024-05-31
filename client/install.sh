bb6a71622baf36cab30f545a0cfe08ca8d39f02f
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-05-31 12:41:06 +0200

    Update install.sh

--- client/install.sh
+++ client/install.sh
@@ -1,10 +1,14 @@
-apt remove docker-compose
-curl -L "https://github.com/docker/compose/releases/download/v2.24.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-apt-get update && sudo apt-get install -y gettext-base
-wget -O /usr/local/bin/check-upgradable-packages.sh  https://github.com/jjziets/gddr6_temps/raw/master/update-package-count.sh;
-chmod +x /usr/local/bin/check-upgradable-packages.sh;
-sudo bash -c '(crontab -l 2>/dev/null; echo "0 * * * * /usr/local/bin/check-upgradable-packages.sh") | crontab -'
-docker-compose pull
-sed "s/__HOST_HOSTNAME__/$(hostname)/g" docker-compose.yml | docker-compose -f - up -d
+# install docker
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository  -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update -y 
+apt install docker.io -y
+
+# install docker-compose
+sudo apt-get install docker-compose-plugin
+docker compose up -d # this will start all server
+
+export HOSTNAME-$(hostname)
+docker compose up -d
