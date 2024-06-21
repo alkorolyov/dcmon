53a19fa02f34c38255d3d4ea3de23ab3c0bab723
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-06-21 15:19:39 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Get user input for SSH connection details
+read -p "Enter SSH username: " user
+read -p "Enter SSH hostname or IP address: " host
+read -p "Enter SSH port: " port
+
 # Get folder names
 folder_name=$(hostname)
 vast_dir="/var/lib/vastai_kaalia"
@@ -23,16 +28,16 @@ mkdir -p /mnt/backup/vast/$folder_name
 # Tar the folder /var/lib/docker and send it to the server
 echo -e "\nSending the tar file to server..."
 
-# cat $vast_dir/host_port_range |  ssh user@host -p port "cat > /mnt/backup/vast/$folder_name/host_port_range"
-# cat $vast_dir/host_port_range |  ssh user@host -p port "cat > /mnt/backup/vast/$folder_name/host_port_range"
-# tar -cf - /var/lib/docker | pv | pzstd - | ssh user@host -p port "cat > /mnt/backup/vast/$folder_name/docker.tar.zst"
+cat $vast_dir/host_port_range |  ssh $user@$host -p $port "cat > /mnt/backup/vast/$folder_name/host_port_range"
+cat $vast_dir/host_port_range |  ssh $user@$host -p $port "cat > /mnt/backup/vast/$folder_name/host_port_range"
+tar -cf - /var/lib/docker | pv | pzstd - | ssh $user@$host -p $port "cat > /mnt/backup/vast/$folder_name/docker.tar.zst"
  
-cp $vast_dir/host_port_range /mnt/backup/vast/$folder_name
-cp $vast_dir/machine_id /mnt/backup/vast/$folder_name
-tar -cf - /var/lib/docker | pv | pzstd - > /mnt/backup/vast/$folder_name/docker.tar.zst
+# cp $vast_dir/host_port_range /mnt/backup/vast/$folder_name
+# cp $vast_dir/machine_id /mnt/backup/vast/$folder_name
+# tar -cf - /var/lib/docker | pv | pzstd - > /mnt/backup/vast/$folder_name/docker.tar.zst
 
-echo -e "\n Restarting services ...!"
-
+# Start services
+echo -e "\n Restarting services ..."
 systemctl start docker
 systemctl start docker.socket
 systemctl start vastai
