50a66e23fb354687c30c55fb51643304a1f36e96
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-06-21 15:07:12 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,7 +4,6 @@
 folder_name=$(hostname)
 vast_dir="/var/lib/vastai_kaalia"
 
-
 #install pv and pixz 
 echo -e "\nInstall pv pzstd"
 apt -qq install pv zstd -y
@@ -12,18 +11,31 @@ apt -qq install pv zstd -y
 # Stop services
 echo -e "\nStopping services..."
 systemctl stop cron
+systemctl stop vastai
 systemctl stop docker.socket
 systemctl stop docker
-systemctl stop vastai
 
 # Check and create folder if it does not exist
 echo -e "\nEnsuring the backup folder exists on remote server..."
 mkdir -p /mnt/backup/vast/$folder_name
+# ssh user@host -p port "mkdir -p /mnt/backup/vast/$folder_name"
 
 # Tar the folder /var/lib/docker and send it to the server
 echo -e "\nSending the tar file to server..."
+
+# cat $vast_dir/host_port_range |  ssh user@host -p port "cat > /mnt/backup/vast/$folder_name/host_port_range"
+# cat $vast_dir/host_port_range |  ssh user@host -p port "cat > /mnt/backup/vast/$folder_name/host_port_range"
+# tar -cf - /var/lib/docker | pv | pzstd - | ssh user@host -p port 'cat > /mnt/backup/vast/$folder_name/docker.tar.zst'
+ 
 cp $vast_dir/host_port_range /mnt/backup/vast/$folder_name
 cp $vast_dir/machine_id /mnt/backup/vast/$folder_name
 tar -cf - /var/lib/docker | pv | pzstd - > /mnt/backup/vast/$folder_name/docker.tar.zst
 
-echo -e "\nDone!"
+echo -e "\n Restarting services ...!"
+
+systemctl start docker
+systemctl start docker.socket
+systemctl start vastai
+systemctl start cron
+
+echo -e "\n Done"
