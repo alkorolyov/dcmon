bb05b727a7e9613d5734c3f8591c97a89b84bf1d
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-09-01 20:56:27 +0200

    Update run_exporters.sh

--- client/run_exporters.sh
+++ client/run_exporters.sh
@@ -3,14 +3,15 @@
 VAR_DIR='/var/lib/node_exporter'
 SLEEP_TIME=15
 
-while true; do 
-    for script in $VAR_DIR/exporters/*.sh; do 
+while true; do
+    for script in $VAR_DIR/exporters/*.sh; do
         {
-            prom_file=\"$VAR_DIR/exporters/\$(basename \${script%.*}).prom\"; 
-            sudo bash \$script > \$prom_file.tmp 2>/dev/null && mv \$prom_file.tmp \$prom_file;
-            rm \$prom_file.tmp
+            #echo -e "executing $script";
+            prom_file="$VAR_DIR/exporters/$(basename ${script%.>
+            sudo bash $script > $prom_file.tmp 2>/dev/null && m>        
         } &
     done
     wait
-    sleep $SLEEP_TIME; 
+    # echo "done";
+    sleep $SLEEP_TIME;
 done
