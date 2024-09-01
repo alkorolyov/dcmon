21e097510d154b4b57d3c3b4fef165d60030158c
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-09-01 21:24:00 +0200

    Update run_exporters.sh

--- client/run_exporters.sh
+++ client/run_exporters.sh
@@ -7,8 +7,8 @@ while true; do
     for script in $VAR_DIR/exporters/*.sh; do
         {
             #echo -e "executing $script";
-            prom_file="$VAR_DIR/exporters/$(basename ${script%.>
-            sudo bash $script > $prom_file.tmp 2>/dev/null && m>        
+            prom_file="$VAR_DIR/exporters/$(basename ${script%.*}).prom"
+            bash $script > $prom_file.tmp 2>/dev/null && mv $prom_file.tmp $prom_file;
         } &
     done
     wait
