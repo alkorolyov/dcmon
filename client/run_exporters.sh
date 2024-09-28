3cbb271360d00b329836d526d8fa7f23661504c2
Alex <>
2024-09-28 12:19:22 +0000

    update

--- client/run_exporters.sh
+++ client/run_exporters.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-VAR_DIR='/var/lib/node_exporter'
-SLEEP_TIME=15  # sleep time in seconds
+#VAR_DIR='/var/lib/node_exporter'
+VAR_DIR=${1:-"."}
+SLEEP_TIME=${2:-"15"}  # sleep time in seconds
+
+
+mkdir $VAR_DIR/proms
 
 while true; do
     for script in $VAR_DIR/exporters/*.sh; do
         {
             #echo -e "executing $script";
-            prom_file="$VAR_DIR/exporters/$(basename ${script%.*}).prom"
+            prom_file="$VAR_DIR/proms/$(basename ${script%.*}).prom"
             bash $script > $prom_file.tmp 2>/dev/null && mv $prom_file.tmp $prom_file;
         } &
     done
