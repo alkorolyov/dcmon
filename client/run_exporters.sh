d1c73e9c43d734906a82b1f7c385eaf0a05bd120
Alex <>
2024-09-28 12:54:36 +0000

    update

--- client/run_exporters.sh
+++ client/run_exporters.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-#VAR_DIR='/var/lib/node_exporter'
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root. Exiting..."
+    exit 1
+fi
+
 VAR_DIR=${1:-"."}
-SLEEP_TIME=${2:-"15"}  # sleep time in seconds
+SLEEP_TIME=${2:-"15"}  # Sleep time in seconds
 
+mkdir -p "$VAR_DIR/proms"
 
-mkdir $VAR_DIR/proms
-
+# Infinite loop to repeatedly execute scripts.
 while true; do
-    for script in $VAR_DIR/exporters/*.sh; do
+    for script in "$VAR_DIR/exporters/"*.sh; do
         {
-            #echo -e "executing $script";
-            prom_file="$VAR_DIR/proms/$(basename ${script%.*}).prom"
-            bash $script > $prom_file.tmp 2>/dev/null && mv $prom_file.tmp $prom_file;
+            prom_file="$VAR_DIR/proms/$(basename "${script%.*}").prom"
+            
+            if timeout $((SLEEP_TIME - 5)) bash "$script" > "$prom_file.tmp" 2>> "$VAR_DIR/errors.log"; then
+                mv "$prom_file.tmp" "$prom_file"
+            elif [ $? -eq 124 ]; then
+                echo "$(date +'%Y-%m-%d %H:%M:%S') - Execution of $script timed out" >> "$VAR_DIR/errors.log"
+            else
+                echo "$(date +'%Y-%m-%d %H:%M:%S') - Execution of $script failed" >> "$VAR_DIR/errors.log"
+            fi
         } &
     done
+
     wait
-    # echo "done";
-    sleep $SLEEP_TIME;
+
+    sleep "$SLEEP_TIME"
 done
